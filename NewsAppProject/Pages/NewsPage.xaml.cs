171ea5e35315e6f158e87ad3e7fca639c4fbdcc8
smolyoshino <coolguygameplayer52@gmail.com>
2023-01-10 08:35:33 -0500

    part 2

--- NewsAppProject/Pages/NewsPage.xaml.cs
+++ NewsAppProject/Pages/NewsPage.xaml.cs
@@ -1,9 +1,25 @@
+using NewsAppProject.Models;
+using NewsAppProject.Services;
+
 namespace NewsAppProject.Pages;
 
 public partial class NewsPage : ContentPage
 {
+	public List<Article> ArticlesList { get; set; }
 	public NewsPage()
 	{
 		InitializeComponent();
+		ArticlesList = new List<Article>();
+	}
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		ApiService apiService = new ApiService();
+		var newsResult = await apiService.GetNews();
+		foreach (var item in newsResult.Articles)
+		{
+			ArticlesList.Add(item);
+		}
+		CvNews.ItemsSource = ArticlesList;
 	}
 }
---
+++ NewsAppProject/Services/ApiService.cs
@@ -0,0 +1,21 @@
+﻿using NewsAppProject.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsAppProject.Services
+{
+    public class ApiService
+    {
+        public async Task<Root> GetNews()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://gnews.io/api/v4/top-headlines?token=2b40790cc1b98b86d142162d2c58019e&topic=breaking-news&lang=en");
+            // deserialization
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
